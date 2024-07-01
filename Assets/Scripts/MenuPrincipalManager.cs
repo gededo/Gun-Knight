fb664f53c69bc5323589555b05f3b1598b8fd1e7
WilliamS-J-S <william1904sj@gmail.com>
2024-07-01 18:15:32 -0300

    Add files via upload

---
+++ Assets/Scripts/MenuPrincipalManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    [SerializeField] private string Leveldojogo;
+    [SerializeField] private GameObject painelMenuInicial;
+    [SerializeField] private GameObject painelopcoes;
+   public void Play() {
+     SceneManager.LoadScene(Leveldojogo);
+   }
+
+   public void Optionsmenu() {
+     painelMenuInicial.SetActive(false);
+     painelopcoes.SetActive(true);
+   }
+
+   public void Closeoptionsmenu() {
+     painelMenuInicial.SetActive(true);
+     painelopcoes.SetActive(false);
+   }
+
+   public void Closethegame() {
+    Debug.Log("Saiu do Jogo");
+     Application.Quit();
+   }
+}
